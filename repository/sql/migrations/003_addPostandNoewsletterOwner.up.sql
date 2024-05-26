4b7974b2521323f0ddb22c30d29c8b4506180d3a
Eliška Vokounová <vokounova.eliska@gmail.com>
2024-05-26 20:58:01 +0200

    add Post to db, implement updateNewsletter

---
+++ repository/sql/migrations/003_addPostandNoewsletterOwner.up.sql
@@ -0,0 +1,21 @@
+ALTER TABLE newsletters
+    ADD COLUMN owner_id uuid;
+
+ALTER TABLE newsletters
+ADD CONSTRAINT fk_owner
+FOREIGN KEY (owner_id)
+REFERENCES profiles (id);
+
+ALTER TABLE profiles
+    ADD COLUMN email text;
+
+CREATE TABLE posts (
+id uuid PRIMARY KEY DEFAULT gen_random_uuid(),
+created_at     TIMESTAMPTZ   NOT NULL,
+title          TEXT          NOT NULL,
+content        TEXT          NOT NULL,
+newsletter_id  uuid          NOT NULL,
+CONSTRAINT fk_newsletter
+FOREIGN KEY (newsletter_id)
+REFERENCES newsletters (id)
+);
