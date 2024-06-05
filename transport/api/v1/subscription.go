ee7fe46c31b796ad936b70b572571006f787717b
Petr Buk <91278935+PetrBuk@users.noreply.github.com>
2024-06-05 11:16:33 +0200

    add confirmation pages (#23)

--- transport/api/v1/subscription.go
+++ transport/api/v1/subscription.go
@@ -2,7 +2,9 @@ package v1
 
 import (
 	"errors"
+	"html/template"
 	"net/http"
+	"os"
 	"vse-go-newsletter-api/service/model"
 	"vse-go-newsletter-api/transport/util"
 )
@@ -27,26 +29,56 @@ func (h *Handler) UnsubscribeNewsletter(w http.ResponseWriter, r *http.Request) 
 
 	email := getEmail(w, r)
 	if email != "" {
-		unsubscription, err := h.service.UnsubscribeNewsletter(r.Context(), newsletterId, email)
+		_, err := h.service.UnsubscribeNewsletter(r.Context(), newsletterId, email)
 		if err != nil {
 			util.WriteErrResponse(w, http.StatusInternalServerError, err)
 			return
 		}
-		util.WriteResponse(w, http.StatusOK, unsubscription)
+
+		tmpl, err := template.ParseFiles("templates/pages/unsubscribe_success.html")
+		if err != nil {
+				util.WriteErrResponse(w, http.StatusInternalServerError, err)
+				return
+		}
+
+		if err := tmpl.Execute(w, nil); err != nil {
+      util.WriteErrResponse(w, http.StatusInternalServerError, err)
+			return
+    }
 	}
 }
 
 func (h *Handler) ConfirmSubscription(w http.ResponseWriter, r *http.Request) {
 	newsletterId := getNewsletterId(w, r)
 	email := getEmail(w, r)
+	baseUrl := os.Getenv("BASE_URL")
+
 	if email != "" {
-		subscription, err := h.service.ConfirmSubscription(r.Context(), newsletterId, email)
+		_, err := h.service.ConfirmSubscription(r.Context(), newsletterId, email)
+
 		if err != nil {
 			util.WriteErrResponse(w, http.StatusInternalServerError, err)
 			return
 		}
-		util.WriteResponse(w, http.StatusOK, model.Subscription{ID: subscription.ID, Email: subscription.Email, NewsletterId: subscription.NewsletterId, CreatedAt: subscription.CreatedAt, IsConfirmed: subscription.IsConfirmed})
 
+		tmpl, err := template.ParseFiles("templates/pages/confirm_success.html")
+		if err != nil {
+				util.WriteErrResponse(w, http.StatusInternalServerError, err)
+				return
+		}
+
+		templateData := struct {
+		SubscriberEmail string
+		UnsubscribeLink string
+	}{
+		SubscriberEmail: email,
+		UnsubscribeLink: baseUrl + "/api/v1/newsletters/" + newsletterId.String() + "/unsubscribe?email=" + email,
+	}
+
+		if err := tmpl.Execute(w, templateData); err != nil {
+      util.WriteErrResponse(w, http.StatusInternalServerError, err)
+			return
+    }
 	}
 
 }
